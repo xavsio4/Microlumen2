7803df32b3630a9696acd1404ac814ac7df3cc45
xavier Villamuera <gzav@fifteenpeas.com>
2020-09-17 11:33:17 +0200

    bugfix

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,4 +1,4 @@
 <h1>Hi,</h1>
 <i>This is an email from your microservice used through {{ $source }}</i>
-<h6>Subject: {{subject}}</h6>
-<p>Content: {{content}}</p>
+<h6>Subject: {{$subject}}</h6>
+<p>Content: {{$content}}</p>
