d612f632358684d7141cf21bdd82410cb62cec0f
xavier Villamuera <gzav@fifteenpeas.com>
2021-03-22 09:31:00 +0100

    no message

--- app/Http/Controllers/GpsController.php
+++ app/Http/Controllers/GpsController.php
@@ -20,7 +20,7 @@ class GpsController extends Controller
     public function filein(request $request)
     {
         $this->validate($request, [
-        'file' => 'required|mimes:application/gpx+xml,text/gpsxml,application/gpsxml,gpx,xml|max:2048'
+        'file' => 'required|mimes:gpx,xml,txt|max:2048'
         ]);
         
         $fileModel = new File;
